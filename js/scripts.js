375876cf702c8307557d6243b67a4ff6c7955bc3
ken <ken.mbira@student.moringaschool.com>
2021-07-16 15:42:05 +0300

    add a list of variables defining the year month day and gender

--- js/scripts.js
+++ js/scripts.js
@@ -9,3 +9,15 @@ function show(show, hidden) {
 var maleAkanNames = [Kwasi, Kwadwo, Kwabena, Kwaku, Yaw, Kofi, Kwame]
     //Female names
 var femaleAkanNames = [Akosua, Adwoa, Abenaa, Akua, Yaa, Afua, Ama]
+
+
+//variable former
+var year = document.getElementById("year").value;
+var month = document.getElementById("month").value;
+var day = document.getElementById("day").value;
+var gender = document.getElementByName("gender").value;
+
+//Month validator
+var monthValidator = () => {
+    if (document)
+}
